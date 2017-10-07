5590223fea994ab67a1b77a43948d0ff35d10f6e
beepbooprobot <benlangford10@gmail.com>
2017-10-07 19:14:09 +0100

    Spicier Stuff

--- src/testTransition.java
+++ src/testTransition.java
@@ -6,10 +6,7 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -23,6 +20,10 @@ import javafx.util.Duration;
 
 
 public class testTransition {
+    private static Group group;
+    private static GridPane boneZone;
+    private static GridPane defaultPane;
+    private static Rectangle transitionScreen;
 
     public static void main(String[] args) {
         new JFXPanel();
@@ -42,22 +43,66 @@ public class testTransition {
         final DoubleProperty leftEdge = new SimpleDoubleProperty(0);
         final DoubleProperty rightEdge = new SimpleDoubleProperty(0);
 
-        Group group = new Group();
+        group = new Group();
         Scene defaultScene = new Scene(group, 480, 500, Color.FORESTGREEN);
         Group group2 = new Group();
-        Scene boneScene = new Scene(group2);
-        GridPane boneZone = new GridPane();
+        boneZone = new GridPane();
         boneZone.setHgap(10);
         boneZone.setVgap(10);
         Image bone = new Image("skeleton-animated-gif-20.gif");
         ImageView bonez = new ImageView(bone);
         Image image = new Image("download.jpg");
         ImageView iv = new ImageView(image);
-        boneZone.getChildren().add(bonez);
+        Button bobob = new Button("Exit the bone zone");
+        bobob.setOnAction(event -> {
+            group.getChildren().add(transitionScreen);
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.ZERO, new KeyValue(leftEdge, 0)),
+                    new KeyFrame(Duration.ZERO, new KeyValue(rightEdge, 0)),
+                    new KeyFrame(Duration.millis(500), new KeyValue(rightEdge, 0)),
+                    new KeyFrame(Duration.millis(1500), new KeyValue(leftEdge, 1)),
+                    new KeyFrame(Duration.millis(2000), new KeyValue(rightEdge, 1)),
+                    new KeyFrame(Duration.millis(2000), new KeyValue(leftEdge, 1)),
+                    new KeyFrame(Duration.millis(2500), new KeyValue(rightEdge, 1)),
+                    new KeyFrame(Duration.millis(2500), new KeyValue(leftEdge, 1)),
+                    new KeyFrame(Duration.millis(3000), new KeyValue(leftEdge, 1)),
+                    new KeyFrame(Duration.millis(3500), new KeyValue(rightEdge, 0)),
+                    new KeyFrame(Duration.millis(4000), new KeyValue(leftEdge, 0)),
+                    new KeyFrame(Duration.millis(4000), new KeyValue(rightEdge, 0))
+            );
+            timeline.play();
+            Task<Void> sleeper = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    try {
+                        Thread.sleep(4500);
+                    } catch (InterruptedException ignored) {
+                    }
+                    return null;
+                }
+            };
+            Task<Void> changeScene = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException ignored) {
+                    }
+                    return null;
+                }
+            };
+            sleeper.setOnSucceeded(event2 -> group.getChildren().remove(transitionScreen));
+            changeScene.setOnSucceeded((event1 -> exitBone()));
+            new Thread(sleeper).start();
+            new Thread(changeScene).start();
+
+        });
+        boneZone.getChildren().addAll(bonez, bobob);
         group2.getChildren().add(boneZone);
 
 
-        Rectangle transitionScreen = new Rectangle(0, 0, 480, 500);
+        transitionScreen = new Rectangle(0, 0, 480, 500);
+        transitionScreen.toFront();
         transitionScreen.setFill(Color.BLACK);
         transitionScreen.setStyle("-fx-fill: linear-gradient(to right, left-col, right-col);");
         group.styleProperty().bind(
@@ -105,13 +150,13 @@ public class testTransition {
                 }
             };
             sleeper.setOnSucceeded(event2 -> group.getChildren().remove(transitionScreen));
-            changeScene.setOnSucceeded((event1 -> iv.setImage(bone)));
+            changeScene.setOnSucceeded((event1 -> enterBone()));
             new Thread(sleeper).start();
             new Thread(changeScene).start();
 
         });
 
-        GridPane defaultPane = new GridPane();
+        defaultPane = new GridPane();
         defaultPane.setHgap(10);
         defaultPane.setVgap(10);
         defaultPane.add(iv, 1, 0);
@@ -121,6 +166,18 @@ public class testTransition {
         group.getChildren().add(defaultPane);
         stage.setScene(defaultScene);
 
+    }
+
+    private static void enterBone() {
+        group.getChildren().remove(defaultPane);
+        group.getChildren().add(boneZone);
+        boneZone.toBack();
+    }
+
+    private static void exitBone() {
+        group.getChildren().remove(boneZone);
+        group.getChildren().add(defaultPane);
+        defaultPane.toBack();
     }
 
 }
